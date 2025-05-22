22062e8918f3b8bd31d17e0cadcf89d4bd1da81d
Akash-singh2684 <akash2604singh@gmail.com>
2025-05-22 17:27:37 +0530

    frontend of zerodha clone

---
+++ src/landing_page/products/LeftSection.js
@@ -0,0 +1,7 @@
+function LeftSection() {
+    return ( 
+        <></>
+     );
+}
+
+export default LeftSection;
---
+++ src/landing_page/products/ProductsPage.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import Universe from "./Universe";
+import RightSection from './RightSection';
+
+function ProductsPage() {
+    return ( 
+       <>
+       <Hero />
+       <LeftSection />
+       <Universe />
+       <RightSection />
+       </>
+      
+     );
+}
+
+export default ProductsPage;
